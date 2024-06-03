6b8c089597a36a8ca1c684a2ffc31ec16364142d
p.teslenko <mr.kognitivist@gmail.com>
2024-06-03 14:43:05 +0400

    ++

--- app/src/main/java/com/kognitivist/androidlessons/MainActivity.kt
+++ app/src/main/java/com/kognitivist/androidlessons/MainActivity.kt
@@ -5,41 +5,49 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kognitivist.androidlessons.ui.theme.AndroidLessonsTheme
 
 class MainActivity : ComponentActivity() {
-    companion object{
+    companion object {
         const val TAG = "TagLifeCycleActivity"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
         enableEdgeToEdge()
-        setContent {}
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {}
+        }
     }
+
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart")
     }
+
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume")
     }
+
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause")
     }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop")
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy")
